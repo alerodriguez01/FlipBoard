85d338d427bba044cc5205707ea1f4586b7d6cec
Alejandro Rodriguez <rodriguezalejandro.anr@gmail.com>
2023-10-18 14:25:08 -0300

    Independencia de prisma e implementacion de ID 5

--- Backend/src/daos/prisma/usuario.dao.ts
+++ Backend/src/daos/prisma/usuario.dao.ts
@@ -1,30 +1,48 @@
 import { Usuario } from '@prisma/client';
-import PrismaSingleton from './dbmanager.js';
 import bcryptjs from 'bcryptjs';
 import { NotFoundError } from '../../excepciones/RepoErrors.js';
+import { PrismaClient } from "@prisma/client";
+import UsuarioDataSource from "../../repositorios/datasource/usuario.datasource.js";
+import PrismaSingleton from "./dbmanager.js";
 
-const prisma = PrismaSingleton.getInstance();
+export class UsuarioPrismaDAO implements UsuarioDataSource {
 
-//Crear usuario
-async function createUser(user: Usuario) {
+  private static INSTANCE: UsuarioPrismaDAO;
+  private prisma: PrismaClient;
 
-  let newUser = {
+  private constructor() {
+    this.prisma = PrismaSingleton.getInstance();
+  }
+
+  public static getInstance(): UsuarioPrismaDAO {
+    if (!UsuarioPrismaDAO.INSTANCE) {
+      UsuarioPrismaDAO.INSTANCE = new UsuarioPrismaDAO();
+    }
+
+    return UsuarioPrismaDAO.INSTANCE;
+  }
+
+  // Crear usuario
+  async createUser(user: Usuario) : Promise<Usuario>{
+
+    let newUser = {
       nombre: user.nombre,
       apellido: user.apellido,
       correo: user.correo,
+    }
+
+    const salt = await this.prisma.salt.findFirst();
+
+    if (!salt) throw new NotFoundError("Salt");
+    const pass = await bcryptjs.hash(user.contrasena, salt.salt);
+
+    const userCreated = await this.prisma.usuario.create({
+      data: { ...newUser, contrasena: pass }
+    });
+
+    return userCreated;
+
   }
 
-  const salt = await prisma.salt.findFirst();
-  
-  if (!salt) throw new NotFoundError("Salt");
-  const pass = await bcryptjs.hash(user.contrasena, salt.salt);
-
-  const userCreated = await prisma.usuario.create({
-    data: {...newUser, contrasena: pass}
-  });
-  
-  return userCreated;
-
+  // demas metodos
 }
-
-export { createUser };
---
+++ Backend/src/repositorios/calificacion.repo.ts
@@ -0,0 +1,23 @@
+import CalificacionDataSource from "./datasource/calificacion.datasource.js";
+import { CalificacionPrismaDAO } from "../daos/prisma/calificacion.dao.js";
+
+
+export class CalificacionRepository implements CalificacionDataSource {
+
+    private static INSTANCE: CalificacionRepository;
+    private calificacionDAO: CalificacionDataSource;
+
+    private constructor() {
+        this.calificacionDAO = CalificacionPrismaDAO.getInstance();
+     }
+
+    public static getInstance(): CalificacionRepository {
+        if (!CalificacionRepository.INSTANCE) {
+            CalificacionRepository.INSTANCE = new CalificacionRepository();
+        }
+
+        return CalificacionRepository.INSTANCE;
+    }
+
+    // metodos
+}
