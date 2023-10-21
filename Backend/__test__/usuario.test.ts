96efce996004af70499df48eb17430395f635c61
elPeiretti <tomaspeiretti@gmail.com>
2023-10-21 19:38:45 -0300

    add parameters test to GET /api/usuarios/:idUsuario

--- Backend/__test__/usuario.test.ts
+++ Backend/__test__/usuario.test.ts
@@ -46,6 +46,7 @@ describe("POST /api/usuarios", () => {
 
 describe("GET /api/usuarios/:idUsuario", () => {
   let user: Response;
+  let curso: Response;
 
   beforeAll( async () => {
     user = await request(app).post('/api/usuarios').send({
@@ -53,6 +54,14 @@ describe("GET /api/usuarios/:idUsuario", () => {
       "apellido": "Pepe",
       "correo": "testmail@gmail.com",
       "contrasena": "passworD123"
+    });
+    curso = await request(app).post('/api/cursos').send({
+      nombre: "curso",
+      tema: "temita",
+      sitioWeb: "google.com",
+      descripcion: "describo descripcion",
+      emailContacto: "unmaildelcurso@gmail.com",
+      docentes: [user.body.id],
     });
   }, 15000);
 
@@ -69,9 +78,32 @@ describe("GET /api/usuarios/:idUsuario", () => {
 
     expect(res.statusCode).toBe(404);
   }, 10000);
+
   test("Intentar obtener usuario inexistente", async () => {
     const res = await request(app).get('/api/usuarios/333333333333333333333333');
 
     expect(res.statusCode).toBe(404);
   }, 10000);
+
+  test("Obtener usuario valido con sus cursos asociados", async () => {
+    const res = await request(app).get('/api/usuarios/'+user.body.id+'?cursos=true');
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.id).toBe(user.body.id);
+    expect(res.body.cursosDocenteModel).toBeTruthy();
+    expect(res.body.cursosAlumnoModel).toBeTruthy();
+    expect(res.body.cursosDocente[0]).toBe(curso.body.id);
+    expect(res.body.cursosDocenteModel[0]).toStrictEqual(curso.body);
+  }, 15000);
+
+  test("Obtener usuario valido con parametro de curso invalido", async () => {
+    const res = await request(app).get('/api/usuarios/'+user.body.id+'?cursos=estoNoEsValido');
+    
+    expect(res.statusCode).toBe(200);
+    expect(res.body.id).toBe(user.body.id);
+    expect(res.body.cursosDocente).toBeTruthy();
+    expect(res.body.cursosDocente[0]).toBe(curso.body.id);
+    expect(res.body.cursosDocenteModel).toBeFalsy();
+    expect(res.body.cursosAlumnoModel).toBeFalsy();
+  }, 15000);
 });
