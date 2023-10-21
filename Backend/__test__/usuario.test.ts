0874d44a9234839b2cfe1fc416e6d62df39343e8
elPeiretti <tomaspeiretti@gmail.com>
2023-10-21 17:45:21 -0300

    add tests for getUsuario method GET /api/usuarios

--- Backend/__test__/usuario.test.ts
+++ Backend/__test__/usuario.test.ts
@@ -1,4 +1,4 @@
-import request from 'supertest';
+import request, { Response } from 'supertest';
 
 const app = 'http://localhost:3100';
 
@@ -41,5 +41,37 @@ describe("POST /api/usuarios", () => {
       "contrasena": "passworD321"
     });
     expect(res.statusCode).toBe(400);
+  }, 10000);
+});
+
+describe("GET /api/usuarios/:idUsuario", () => {
+  let user: Response;
+
+  beforeAll( async () => {
+    user = await request(app).post('/api/usuarios').send({
+      "nombre": "Juan",
+      "apellido": "Pepe",
+      "correo": "testmail@gmail.com",
+      "contrasena": "passworD123"
+    });
+  }, 15000);
+
+  test("Obtener usuario valido", async () => {
+    const res = await request(app).get('/api/usuarios/'+user.body.id);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.id).toBe(user.body.id);
+    expect(res.body.nombre).toBe(user.body.nombre);
+  }, 10000);
+
+  test("Intentar obtener usuario invalido", async () => {
+    const res = await request(app).get('/api/usuarios/999');
+
+    expect(res.statusCode).toBe(404);
+  }, 10000);
+  test("Intentar obtener usuario inexistente", async () => {
+    const res = await request(app).get('/api/usuarios/333333333333333333333333');
+
+    expect(res.statusCode).toBe(404);
   }, 10000);
 });
