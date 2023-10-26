a2ed50ac3275e12398acf05dd280d7dbceae7183
elPeiretti <tomaspeiretti@gmail.com>
2023-10-26 15:21:28 -0300

    add POST /cursos/calificaciones/grupos/:idGrupo tests

--- Backend/__test__/calificacion.test.ts
+++ Backend/__test__/calificacion.test.ts
@@ -228,7 +228,167 @@ describe("POST /cursos/:idCurso/calificaciones", () => {
   });
 
   describe("POST /cursos/calificaciones/grupos/:idGrupo", () => {
-    
+    test("Crear nueva calificacion para un grupo con observaciones", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [0, 1],
+        observaciones: "bueN trabajo",
+        idRubrica: rubrica.body.id,
+        idDocente: docente.body.id
+      });
+
+      expect(res.statusCode).toBe(201);
+      expect(res.body.valores).toEqual([0,1]);
+      expect(res.body.grupoId).toBe(grupo.body.id);
+      expect(res.body.cursoId).toBe(curso.body.id);
+      expect(res.body.observaciones).toBe("bueN trabajo");
+      
+    }, 15000);
+
+    test("Crear nueva calificacion para un grupo sin observaciones", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [0, 2],
+        idRubrica: rubrica.body.id,
+        idDocente: docente.body.id
+      });
+
+      expect(res.statusCode).toBe(201);
+      expect(res.body.valores).toEqual([0,2]);
+      expect(res.body.grupoId).toBe(grupo.body.id);
+      expect(res.body.cursoId).toBe(curso.body.id);
+      expect(res.body.observaciones).toBeFalsy();
+      
+    }, 15000);
+
+    test("Intentar crear una calificacion para un grupo que no pertenece al curso", async () => {
+            const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/123456789123456789123456`).send({
+        valores: [2, 2],
+        idRubrica: rubrica.body.id,
+        idDocente: docente.body.id
+      });
+
+      expect(res.statusCode).toBe(404);
+
+    }, 20000);
+
+    test("Intentar crear una calificacion sin ser un docente del curso", async () => {
+     
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [0, 0],
+        idRubrica: rubrica.body.id,
+        idDocente: alumno1.body.id
+      });
+
+      expect(res.statusCode).toBe(404);
+
+    }, 20000);
+
+    test("Intentar crear una calificacion con valores faltantes", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [2],
+        idRubrica: rubrica.body.id,
+        idDocente: docente.body.id
+      });
+
+      expect(res.statusCode).toBe(400);
+
+    }, 15000);
+
+    test("Intentar crear una calificacion con valores demas", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [2,2,2,2,2],
+        idRubrica: rubrica.body.id,
+        idDocente: docente.body.id
+      });
+
+      expect(res.statusCode).toBe(400);
+
+    }, 15000);
+
+    test("Intentar crear una calificacion con valores fuera de rango", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [2,3],
+        idRubrica: rubrica.body.id,
+        idDocente: docente.body.id
+      });
+
+      expect(res.statusCode).toBe(400);
+
+    }, 15000);
+
+    test("Intentar crear una calificacion sin rubrica asociada", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [2, 2],
+        idDocente: docente.body.id
+      });
+
+      expect(res.statusCode).toBe(400);
+
+    }, 15000);
+
+    test("Intentar crear una calificacion con rubrica inexistente", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [2, 2],
+        idRubrica: "333333333333333333333333",
+        idDocente: docente.body.id
+      });
+
+      expect(res.statusCode).toBe(404);
+
+    }, 15000);
+
+    test("Intentar crear una calificacion con rubrica invalida", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [2, 2],
+        idRubrica: "noValido",
+        idDocente: docente.body.id
+      });
+
+      expect(res.statusCode).toBe(400);
+
+    }, 15000);
+
+    test("Intentar crear una calificacion en un curso invalido", async () => {
+      const res = await request(app).post(`/api/cursos/verdura/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [2, 2],
+        idRubrica: rubrica.body.id,
+        idDocente: docente.body.id
+      });
+
+      expect(res.statusCode).toBe(400);
+
+    }, 15000);
+
+    test("Intentar crear una calificacion sin idDocente", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [2,2],
+        idRubrica: rubrica.body.id,
+      });
+
+      expect(res.statusCode).toBe(400);
+
+    }, 15000);
+
+    test("Intentar crear una calificacion con idDocente invalido", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [2,2],
+        idRubrica: rubrica.body.id,
+        idDocente: "Invalid"
+      });
+
+      expect(res.statusCode).toBe(400);
+
+    }, 15000);
+
+    test("Intentar crear una calificacion con idDocente inexistente", async () => {
+      const res = await request(app).post(`/api/cursos/${curso.body.id}/calificaciones/grupos/${grupo.body.id}`).send({
+        valores: [2,2],
+        idRubrica: rubrica.body.id,
+        idDocente: "333333333333333333333333"
+      });
+
+      expect(res.statusCode).toBe(400);
+
+    }, 15000);
   });
 
 });
