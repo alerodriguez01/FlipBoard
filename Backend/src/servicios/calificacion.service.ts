cca066c8730600ad3f527e9d35c201c3e3aa8f58
elPeiretti <tomaspeiretti@gmail.com>
2023-10-26 15:02:55 -0300

    add usuario belonging to curso check

--- Backend/src/servicios/calificacion.service.ts
+++ Backend/src/servicios/calificacion.service.ts
@@ -3,9 +3,11 @@ import { Calificacion } from "@prisma/client";
 import { CalificacionRepository } from "../persistencia/repositorios/calificacion.repo.js";
 import { RubricaRepository } from "../persistencia/repositorios/rubrica.repo.js";
 import { InvalidValueError, NotFoundError } from "../excepciones/RepoErrors.js";
+import { CursoRepository } from "../persistencia/repositorios/curso.repo.js";
 
 const califcacionRepository = CalificacionRepository.getInstance();
 const rubricaRepository = RubricaRepository.getInstance();
+const cursoRepository = CursoRepository.getInstance();
 
 async function getCalificacionesFromUser(idCurso: string, idUsuario: string, rubrica: boolean, limit: number, offset: number) {
 
@@ -19,6 +21,15 @@ async function getCalificacionesFromUser(idCurso: string, idUsuario: string, rub
 */
 async function createCalificacion(calificacion: Calificacion) {
 
+    // Verificar que el usuario pertenezca al curso
+    
+    const curso = await cursoRepository.getCursoById(calificacion.cursoId);
+    if(!curso)
+        throw new NotFoundError("Curso");
+
+    if(calificacion.usuarioId && !curso.participantes.includes(calificacion.usuarioId))
+        throw new NotFoundError("Usuario");
+    
     // Verificacion adicional para chequear que
     // - la dimension de valores se corresponda con la cantidad de criterios de la rubrica
     // - cada valor este entre 0 y length de niveles de la rubrica
