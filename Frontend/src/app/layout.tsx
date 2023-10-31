dbff20f56695897cb40cc9c646931a1b17eeb9d7
Alejandro Rodriguez <rodriguezalejandro.anr@gmail.com>
2023-10-31 09:52:54 -0300

    add useUser() hook and middleware for protecting routes

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
-import './globals.css'
+import '@/app/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import UserProvider from '@/app/componentes/UserProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-        <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <UserProvider>
+          {children}
+        </UserProvider>
+      </body>
     </html>
   )
 }
