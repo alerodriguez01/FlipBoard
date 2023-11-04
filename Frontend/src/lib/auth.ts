269c5a01835a818219cd4f949e96ddbd0406010b
Alejandro Rodriguez <rodriguezalejandro.anr@gmail.com>
2023-11-04 15:57:23 -0300

    variable de entorno de backend cambiada (por temas de docker)

--- Frontend/src/lib/auth.ts
+++ Frontend/src/lib/auth.ts
@@ -6,7 +6,7 @@ async function login(correo: string, contrasena: string) {
     }
 
     try {
-        const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/api/auth/login', {
+        const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL_CONTAINER + '/api/auth/login', {
             body: JSON.stringify(data),
             credentials: 'include', // para que el browser guarde la cookie del JWT
             headers: {
