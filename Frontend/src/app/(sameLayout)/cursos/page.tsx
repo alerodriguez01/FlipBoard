917454f374522dc1728e5c81122d9b610650e504
elPeiretti <tomaspeiretti@gmail.com>
2023-11-05 19:57:20 -0300

    improve color assignation for CursoCards in /cursos

--- Frontend/src/app/(sameLayout)/cursos/page.tsx
+++ Frontend/src/app/(sameLayout)/cursos/page.tsx
@@ -12,6 +12,7 @@ export default function Cursos() {
 
   const {data: session, status} = useSession();
   const {data, error, isLoading} = useSWR( session ? process.env.NEXT_PUBLIC_BACKEND_URL+endpoints.getUserWithCursos(session.user.id) : null, (url) => fetch(url).then(res => res.json()));
+  let color = 0;
 
   if(error) return (<h1>{error.message}</h1>);
 
@@ -21,8 +22,8 @@ export default function Cursos() {
     :
     <section className=" bg-gray-300 flex flex-1 p-24 gap-5">
       {
-        data.cursosDocenteModel.map((c: Curso) => <CursoCard title={c.nombre} cursoId={c.id} color={Math.floor(Math.random()*3)} editable/>).concat(
-          data.cursosAlumnoModel.map((c: Curso) => <CursoCard title={c.nombre} cursoId={c.id} color={Math.floor(Math.random()*3)}/>)
+        data.cursosDocenteModel.map((c: Curso) => <CursoCard title={c.nombre} cursoId={c.id} color={color++%3} editable/>).concat(
+          data.cursosAlumnoModel.map((c: Curso) => <CursoCard title={c.nombre} cursoId={c.id} color={color++%3}/>)
         )
       }
     </section>
