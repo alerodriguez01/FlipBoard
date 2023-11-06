ce5209f4324fccca7246b2446a9280cc10446d91
elPeiretti <tomaspeiretti@gmail.com>
2023-11-06 12:23:23 -0300

    add form & useForm to CrearCursoModal

--- Frontend/src/app/componentes/CrearCursoModal.tsx
+++ Frontend/src/app/componentes/CrearCursoModal.tsx
@@ -1,8 +1,39 @@
+'use client'
 import React from "react";
 import { Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Spinner } from "./ui/Spinner";
+
+const cursoSchema = z.object({
+  nombre: z.string().max(50, "El nombre no puede contener más de 50 caracteres"),
+  contacto: z.string().email("El correo electrónico es invalido."),
+});
+
+type CursoForm = z.infer<typeof cursoSchema> & { erroresExternos?: string };
 
 
 const CrearCursoModal = (props: {isOpen: boolean, onOpenChange: any}) => {
+
+
+    const {
+      register,
+      handleSubmit,
+      formState: {
+          errors,
+          isSubmitting
+      },
+      setError
+  } = useForm<CursoForm>({
+      resolver: zodResolver(cursoSchema)
+  });
+
+  const onSubmit = async (onClose: Function) => {
+
+  };
+  
+
   return (
     <Modal
         isOpen={props.isOpen}
@@ -12,44 +43,62 @@ const CrearCursoModal = (props: {isOpen: boolean, onOpenChange: any}) => {
             {(onClose) => (
               <>
                 <ModalHeader className="flex flex-col gap-1">Crear nuevo curso</ModalHeader>
-                <ModalBody>
-                  <Input
-                    autoFocus
-                    variant="bordered"
-                    label="Nombre"
-                    placeholder="Nombre del curso"
-                    isRequired/>
+                <form action="" onSubmit={handleSubmit(() => onSubmit(onClose))}>
+                  <ModalBody>
                     <Input
-                    autoFocus
-                    variant="bordered"
-                    label="Tema"
-                    placeholder="Tema del curso" />
+                      autoFocus
+                      variant="bordered"
+                      label="Nombre"
+                      placeholder="Nombre del curso"
+                      isRequired
+                      isInvalid={!!errors.nombre}
+                      errorMessage={errors.nombre?.message}
+                      {...register("nombre")}/>
                     <Input
-                    autoFocus
-                    variant="bordered"
-                    label="Descripción"
-                    placeholder="Descripción del curso" />
+                      autoFocus
+                      variant="bordered"
+                      label="Tema"
+                      placeholder="Tema del curso" />
                     <Input
-                    autoFocus
-                    variant="bordered"
-                    label="Sitio web"
-                    placeholder="Sitio web del cruso" />
+                      autoFocus
+                      variant="bordered"
+                      label="Descripción"
+                      placeholder="Descripción del curso" />
                     <Input
-                    autoFocus
-                    variant="bordered"
-                    label="Contacto"
-                    placeholder="Correo electrónico de contacto"
-                    isRequired />
-                </ModalBody>
-                <ModalFooter className="flex flex-row justify-around">
-                  <p className="text-red-600 place-self-center">* Campos obligatorios</p>
-                  <Button className="bg-[#181e25] text-white">Crear curso</Button>
-                </ModalFooter>
+                      autoFocus
+                      variant="bordered"
+                      label="Sitio web"
+                      placeholder="Sitio web del cruso" />
+                    <Input
+                      autoFocus
+                      variant="bordered"
+                      label="Contacto"
+                      placeholder="Correo electrónico de contacto"
+                      isRequired
+                      isInvalid={!!errors.contacto}
+                      errorMessage={errors.contacto?.message}
+                      {...register("contacto")}/>
+                  </ModalBody>
+
+                  <input type="text" className="hidden" {...register("erroresExternos")} />
+                  {errors.erroresExternos &&
+                      <p className="text-red-500 text-sm">{`${errors.erroresExternos.message}`}</p>}
+
+                  <ModalFooter className="flex flex-row justify-around">
+                    <p className="text-red-600 place-self-center">* Campos obligatorios</p>
+                    <Button className="bg-[#181e25] text-white"
+                     isLoading={isSubmitting}
+                      type="submit"
+                      spinner={Spinner}
+                    >Crear curso</Button>
+                  </ModalFooter>
+                  
+                  </form>
               </>
             )}
 
           </ModalContent>
-      </Modal>
+    </Modal>
   )
 }
 
