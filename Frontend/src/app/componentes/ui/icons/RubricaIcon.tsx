3da3b8425dfaf83ad65e7a89d681508aa38112f8
Alejandro Rodriguez <rodriguezalejandro.anr@gmail.com>
2023-11-06 15:56:56 -0300

    change rubrica icon

--- Frontend/src/app/componentes/ui/icons/RubricaIcon.tsx
+++ Frontend/src/app/componentes/ui/icons/RubricaIcon.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-export const RubricaIcon = (props: {toggle?: boolean}) => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" opacity={props.toggle ? 1 : 0.3}>
-    <path d="M80-120v-480h220v480H80Zm290 0v-720h220v720H370Zm290 0v-400h220v400H660Z"/>
+export const RubricaIcon = (props: { toggle?: boolean, theme: 'light' | 'dark' }) => (
+  <svg
+    fill={props.theme === 'light' ? "#000000" : "#ffffff"}
+    width="18"
+    height="18"
+    version="1.1"
+    id="Capa_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 490 490"
+    xmlSpace="preserve"
+    stroke="#000000">
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier"> <g> <g>
+      <rect y="162.2" width="117.8" height="327.8"> </rect>
+      <rect x="186.3" width="117.8" height="490"></rect>
+      <rect x="372.2" y="82.4" width="117.8" height="407.6"></rect>
+    </g>
+    </g>
+    </g>
   </svg>
 );
