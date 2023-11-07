f736c1067c91149c54433b4c6f8cc9b8da4da93d
Alejandro Rodriguez <rodriguezalejandro.anr@gmail.com>
2023-11-07 08:53:31 -0300

    fix bug on /cursos and change conditional render on /murales

--- Frontend/src/app/(sameLayout)/cursos/[idCurso]/murales/page.tsx
+++ Frontend/src/app/(sameLayout)/cursos/[idCurso]/murales/page.tsx
@@ -7,22 +7,24 @@ import { useSession } from "next-auth/react";
 import useSWR from "swr";
 
 export default function Murales({ params }: { params: { idCurso: string } }) {
-  
-  const {data: session, status} = useSession();
-  const {data, error, isLoading} = useSWR( session ? process.env.NEXT_PUBLIC_BACKEND_URL+endpoints.getAllMuralesWithRubricas(params.idCurso) : null, (url) => fetch(url).then(res => res.json()));
+
+  const { data: session, status } = useSession();
+  const { data, error, isLoading } = useSWR(session ? process.env.NEXT_PUBLIC_BACKEND_URL + endpoints.getAllMuralesWithRubricas(params.idCurso) : null, (url) => fetch(url).then(res => res.json()));
   let color = 0;
 
-  if(error) return (<h1>{error.message}</h1>);
-  
+  if (error) return (<h1>{error.message}</h1>);
+
+  if (status === 'loading' || isLoading)
+    return <Spinner color="primary" size="lg" className="justify-center items-center h-full" />
+
   return (
-    status === 'loading' || isLoading ?
-      <Spinner color="primary" size="lg" className="justify-center items-center h-full" />
-    :
     <section className="flex flex-wrap overflow-auto items-center gap-6 p-8">
       {
-        data.length < 1 ? <h3>No hay murales</h3> :
-        data.map((m: Mural) => 
-        <MuralCard title={m.nombre} muralId={m.id} rubrica={m.rubricaModel?.nombre} color={color++%3} editable={session?.user.cursosDocente.includes(m.cursoId)}/>)
+        data.length < 1 ?
+          <h3>No hay murales</h3>
+          :
+          data.map((m: Mural) =>
+            <MuralCard title={m.nombre} muralId={m.id} rubrica={m.rubricaModel?.nombre} color={color++ % 3} editable={session?.user.cursosDocente.includes(m.cursoId)} />)
 
       }
     </section>
