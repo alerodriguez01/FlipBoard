f8fc5fb0093eb7e3396926c60e68b168e0f6d30e
elPeiretti <tomaspeiretti@gmail.com>
2023-11-09 15:37:15 -0300

    change param participante to nombre in /api/cursos/{idCurso}/grupos

--- Backend/src/persistencia/datasource/grupo.datasource.ts
+++ Backend/src/persistencia/datasource/grupo.datasource.ts
@@ -6,7 +6,7 @@ type PaginatedGrupos = {
 }
 
 export default interface GrupoDataSource {
-    getGruposFromCurso(idCurso: string, integrante: string, limit: number, offset: number): Promise<PaginatedGrupos | null>;
+    getGruposFromCurso(idCurso: string, nombre: string, limit: number, offset: number): Promise<PaginatedGrupos | null>;
     createGrupo(grupo: Grupo) : Promise<Grupo | null>;
     getGrupoById(idGrupo: string): Promise<Grupo | null>;
     // ir agregando métodos restantes 
