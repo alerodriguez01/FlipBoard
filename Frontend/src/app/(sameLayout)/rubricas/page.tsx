b5ed26f7b3be3dfa42964456466961ac422ba8f3
elPeiretti <tomaspeiretti@gmail.com>
2023-11-11 17:03:31 -0300

    add name filter to ver rubricas

--- Frontend/src/app/(sameLayout)/rubricas/page.tsx
+++ Frontend/src/app/(sameLayout)/rubricas/page.tsx
@@ -1,56 +1,19 @@
-'use client';
-import { RubricaGrid } from "@/app/componentes/ui/RubricaGrid";
+'use client'
+import { RubricasAccordion } from "@/app/componentes/ui/RubricasAccordion";
 import { PlusIcon } from "@/app/componentes/ui/icons/PlusIcon";
-import { SearchIcon } from "@/app/componentes/ui/icons/SearchIcon";
-import endpoints from "@/lib/endpoints";
-import { Rubrica } from "@/lib/types";
-import { Accordion, AccordionItem, Button, Divider, Input } from "@nextui-org/react";
+import { Button, Spinner } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
-import { useTheme } from "next-themes";
-import { useState } from "react";
-import useSWR from "swr";
 
 export default function Rubricas() {
 
-    const {theme} = useTheme();
-    const currentTheme = theme === "dark" ? "dark" : "light";
-    const { data: session, status } = useSession();
-    const { data, error, isLoading } = useSWR(session ? process.env.NEXT_PUBLIC_BACKEND_URL + endpoints.getAllRubricasFromUser(session.user.id) : null,
-        (url) => fetch(url).then(res => res.json()));
+    const { data: session, status } = useSession();    
+
+    if (status === 'loading' || !session?.user)
+        return <Spinner color="primary" size="lg" className="justify-center items-center h-full" />
 
     return (
-        <section className="m-5">
-            <header className="flex flex-row justify-between mb-5">
-                <h1 className="text-2xl font-semibold">Rúbricas</h1>
-                <Input
-                radius="none"
-                variant="underlined"
-                size="lg"
-                placeholder={'Buscar rúbrica'}
-                startContent={<SearchIcon theme={currentTheme}/>}
-                className="w-[400px]" />
-            </header>
-            
-            <Accordion variant="splitted">
-                {data?.map((rubric: Rubrica) => (
-                <AccordionItem 
-                    key={rubric.id} 
-                    title={
-                        <div className="flex flex-row justify-between">
-                            <h1 className="self-center">{rubric.nombre}</h1>
-                            <div className="flex gap-3">
-                                <Button className="">Modificar</Button>
-                                <Button>Eliminar</Button>
-                            </div>
-                        </div>
-                    }
-                    textValue={`Rubrica ${rubric.nombre}`}
-                >
-                    <RubricaGrid evaluable={false} label={rubric.nombre} criterios={rubric.criterios} niveles={rubric.niveles}/>
-                </AccordionItem>
-                ))}
-            </Accordion>
-
+        <section>
+            <RubricasAccordion userId={session.user.id}/>
             <Button
                 className="bg-[#181e25] text-white fixed bottom-10 right-10"
                 startContent={<PlusIcon color="#FFFFFF" />}
---
+++ Frontend/src/app/componentes/ui/RubricasAccordion.tsx
@@ -0,0 +1,57 @@
+import { Accordion, AccordionItem, Button, Input } from "@nextui-org/react";
+import React, { useState } from "react";
+import { SearchIcon } from "./icons/SearchIcon";
+import { useTheme } from "next-themes";
+import { Rubrica } from "@/lib/types";
+import { RubricaGrid } from "./RubricaGrid";
+import endpoints from "@/lib/endpoints";
+import useSWR from "swr";
+
+const RubricasAccordion = (props: {userId: string}) => {
+
+  const {theme} = useTheme();
+  const currentTheme = theme === "dark" ? "dark" : "light";
+  const [nombre, setNombre] = useState("");
+  const { data, error, isLoading } = useSWR(process.env.NEXT_PUBLIC_BACKEND_URL + endpoints.getAllRubricasFromUser(props.userId) + `?nombre=${nombre}`,
+      (url) => fetch(url).then(res => res.json()));
+    
+  if (error) return (<h1>{error.message}</h1>);
+  
+  return (
+    <section className="m-5">
+        <header className="flex flex-row justify-between mb-5">
+            <h1 className="text-2xl font-semibold">Rúbricas</h1>
+            <Input
+            radius="none"
+            variant="underlined"
+            size="lg"
+            placeholder={'Buscar rúbrica'}
+            startContent={<SearchIcon theme={currentTheme}/>}
+            className="w-[400px]"
+            onValueChange={(value) => setNombre(value)} />
+        </header>
+        
+        <Accordion variant="splitted">
+            {data?.map((rubric: Rubrica) => (
+            <AccordionItem 
+                key={rubric.id} 
+                title={
+                    <div className="flex flex-row justify-between">
+                        <h1 className="self-center">{rubric.nombre}</h1>
+                        <div className="flex gap-3">
+                            <Button className="">Modificar</Button>
+                            <Button>Eliminar</Button>
+                        </div>
+                    </div>
+                }
+                textValue={`Rubrica ${rubric.nombre}`}
+            >
+                <RubricaGrid evaluable={false} label={rubric.nombre} criterios={rubric.criterios} niveles={rubric.niveles}/>
+            </AccordionItem>
+            ))}
+        </Accordion>
+      </section>
+  );
+}
+
+export { RubricasAccordion };
