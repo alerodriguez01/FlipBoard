39b5b54b6880ed0f80d227bfaa5ceb915fa55e9c
Alejandro Rodriguez <rodriguezalejandro.anr@gmail.com>
2023-11-16 22:05:48 -0300

    fix error: Prop fill did not match.

--- Frontend/src/app/componentes/ui/SwitchTheme.tsx
+++ Frontend/src/app/componentes/ui/SwitchTheme.tsx
@@ -2,12 +2,25 @@
 import { useTheme } from "next-themes";
 import { Button } from "@nextui-org/react";
 import DarkLightIcon from "./icons/DarkLightIcon";
+import { useEffect, useState } from "react";
 
 
 const SwitchTheme = () => {
 
-    const { theme, setTheme } = useTheme();
+    // https://www.npmjs.com/package/next-themes#avoid-hydration-mismatch
+    const [mounted, setMounted] = useState(false)
+    const { theme, setTheme } = useTheme()
     const currentTheme = theme === "dark" ? "dark" : "light"
+
+
+    // useEffect only runs on the client, so now we can safely show the UI
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
 
     return (
         <Button
