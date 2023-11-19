12c28031b722ae3d4bf512ff2864a1117a935bdd
elPeiretti <tomaspeiretti@gmail.com>
2023-11-19 13:43:01 -0300

    add state to CriterioCard

--- Frontend/src/app/componentes/ui/CriterioCard.tsx
+++ Frontend/src/app/componentes/ui/CriterioCard.tsx
@@ -1,18 +1,53 @@
 import { Button, Card, CardBody, Input, Textarea } from "@nextui-org/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CrossIcon } from "./icons/CrossIcon";
+import { useController } from "react-hook-form";
 
-const CriterioCard = (props: {id: string, niveles: string[], onDelete?: (id: string) => void}) => {
+type CriterioProps = {
+  id: string,
+  niveles: string[],
+  onDelete?: (id: string) => void,
+  control?: any,
+  name?: string
+}
 
+const CriterioCard = (props: CriterioProps) => {
+
+
+  const [descripciones, setDescripciones] = useState(new Map());
+  const [nombre, setNombre] = useState("");
+
+  const {
+    field = undefined,
+    fieldState: {invalid = undefined, error = undefined}
+  } = props.control && props.name ? useController({name: props.name, control: props.control}) : {fieldState: {}};
+
+  useEffect(() => {
+    let removed = new Map();
+    props.niveles.forEach(n => removed.set(n, descripciones.get(n)));
+    setDescripciones(removed);
+  }, [props.niveles])
+
+  console.log(props.niveles);
+  console.log(descripciones);
   return (
     <Card>
       <CardBody>
         <header className="flex flex-row justify-between mb-3">
-          <Input variant="underlined" className="max-w-[500px]" placeholder="Nombre del criterio"/>
+          <Input defaultValue={nombre} onValueChange={value => setNombre(value)} variant="underlined" className="max-w-[500px]" placeholder="Nombre del criterio"/>
           <Button className="self-center" size="sm" isIconOnly variant="light" onPress={() => props.onDelete?.(props.id)}><CrossIcon/></Button>
         </header>
         <div className="flex flex-row gap-3">
-          {props.niveles.map(n => <Textarea placeholder="Descripción del nivel" variant="bordered" className="w-[300px]" size="sm"/>)}
+          {props.niveles.map(n => {
+            console.log(descripciones.get(n));
+            return <Textarea 
+              key={n}
+              defaultValue={descripciones.get(n)}
+              onValueChange={value => setDescripciones(prev => prev.set(n,value))} 
+              placeholder="Descripción del nivel"
+              variant="bordered" className="w-[300px]" size="sm"
+            />})
+          }
         </div>
         
       </CardBody>
