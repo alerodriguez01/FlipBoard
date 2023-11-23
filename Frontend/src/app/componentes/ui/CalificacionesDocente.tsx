22826acbd6f026d508e930feacc55eef978670b1
elPeiretti <tomaspeiretti@gmail.com>
2023-11-23 11:18:34 -0300

    add tabs to CalificacionesDocente

--- Frontend/src/app/componentes/ui/CalificacionesDocente.tsx
+++ Frontend/src/app/componentes/ui/CalificacionesDocente.tsx
@@ -1,3 +1,4 @@
+import { Tab, Tabs } from '@nextui-org/react'
 import React from 'react'
 
 type CalificacionesDocenteProps = {
@@ -7,7 +8,14 @@ type CalificacionesDocenteProps = {
 
 const CalificacionesDocente = ({ idCurso, idDocente }: CalificacionesDocenteProps) => {
   return (
-    <div>CalificacionesDocente</div>
+    <section className='flex flex-col'>
+      <Tabs variant='underlined' classNames={{base: "w-full", cursor:'w-full', tabList:"w-full justify-between",tab: "w-full h-[50px] text-xl"}}>
+        <Tab key="mural" title="Mural"/>
+        <Tab key="alumno" title="Alumno"/>
+        <Tab key="grupo" title="Grupo"/>
+      </Tabs>
+      
+    </section>
   )
 }
 
