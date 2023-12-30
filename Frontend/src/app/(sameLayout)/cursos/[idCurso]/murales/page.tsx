7c98f694b27aa01aa886f651de6b0256977d6bd3
elPeiretti <tomaspeiretti@gmail.com>
2023-12-30 12:20:48 -0300

    implement warningModal state handling

--- Frontend/src/app/(sameLayout)/cursos/[idCurso]/murales/page.tsx
+++ Frontend/src/app/(sameLayout)/cursos/[idCurso]/murales/page.tsx
@@ -8,7 +8,7 @@ import WarningModal from "@/app/componentes/ui/WarningModal";
 import { PlusIcon } from "@/app/componentes/ui/icons/PlusIcon";
 import endpoints from "@/lib/endpoints";
 import { Curso, Mural, Rubrica } from "@/lib/types";
-import { Button, Spinner, useDisclosure } from "@nextui-org/react";
+import { Button, Checkbox, Spinner, useDisclosure } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
@@ -36,6 +36,7 @@ export default function Murales({ params }: { params: { idCurso: string } }) {
   
   const { isOpen: isWarningOpen, onOpen: onWarningOpen, onOpenChange: onWarningOpenChange } = useDisclosure();
   const [warningRedirect, setWarningRedirect] = useState<() => void>(() => {});
+  const [warningDontShow, setWarningDontShow] = useState(false);
 
   const [search, setSearch] = useState("");
 
@@ -103,6 +104,14 @@ export default function Murales({ params }: { params: { idCurso: string } }) {
   }
 
   const handleCardPress = (redirect: () => void) => {
+
+    const showWarning = localStorage.getItem("showMuralWarning");
+
+    if(showWarning === "skip") {
+      redirect();
+      return;
+    }
+
     setWarningRedirect(() => redirect);
     onWarningOpen();
   }
@@ -176,7 +185,30 @@ export default function Murales({ params }: { params: { idCurso: string } }) {
 
       <EliminarModal isOpen={isEliminarOpen} onOpenChange={onEliminarOpenChange} type="mural" entityName={selectedMural?.nombre || ""} onEliminar={onEliminarMural} extraMessage="NOTA: Se eliminará el mural y todas las calificaciones asociadas."/>
 
-      <WarningModal isOpen={isWarningOpen} onOpenChange={onWarningOpenChange} onConfirm={warningRedirect}/>
+      <WarningModal
+        isOpen={isWarningOpen}
+        onOpenChange={onWarningOpenChange}
+        onConfirm={() => {localStorage.setItem("showMuralWarning", warningDontShow ? "skip" : "show"); warningRedirect();}}
+        message={
+          <>
+            <p className="text-justify">
+              Usted se encuentra a punto de ingresar a un mural colaborativo, en donde múltiples usuarios pueden participar.
+            </p>
+            <p className="text-justify">
+              Le recomendamos que acuerde con el docente un color para identificar sus cambios y evitar conflictos. 
+                Además, le recomendamos asentar su nombre o los nombres de los integrantes de su grupo cerca de los cambios que realice.
+            </p>
+          </>
+        }
+        leftFooterContent={
+          <Checkbox classNames={{label: "text-gray-400"}} radius="none" size="sm" color="primary"
+            onValueChange={(val) => setWarningDontShow(val)}
+          >
+            No volver a mostrar
+          </Checkbox>
+        }
+      />
+
       {isDocente &&
         <>
           <CrearModificarMuralModal
