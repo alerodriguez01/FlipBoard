a3084e0105f4f4cf6d24727ca929db543510e28b
Alejandro N. Rodriguez <rodriguezalejandro.anr@gmail.com>
2024-01-06 22:40:10 -0300

    feat: allow download all Calificaciones from a given Curso

--- Frontend/src/app/(sameLayout)/cursos/[idCurso]/calificaciones/page.tsx
+++ Frontend/src/app/(sameLayout)/cursos/[idCurso]/calificaciones/page.tsx
@@ -2,7 +2,10 @@
 import CalificacionesAlumno from "@/app/componentes/ui/CalificacionesAlumno";
 import CalificacionesDocente from "@/app/componentes/ui/CalificacionesDocente";
 import PagesHeader from "@/app/componentes/ui/PagesHeader";
-import { Spinner } from "@nextui-org/react";
+import { DownloadIcon } from "@/app/componentes/ui/icons/DownloadIcon";
+import { InfoIcon } from "@/app/componentes/ui/icons/InfoIcon";
+import endpoints from "@/lib/endpoints";
+import { Button, Spinner, Tooltip } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 
@@ -10,6 +13,34 @@ export default function Calificaciones({ params: { idCurso } }: { params: { idCu
 
     const { data: session, status } = useSession();
     const isDocente = session?.user.cursosDocente.includes(idCurso)
+
+    const downloadCsv = async () => {
+        try {
+            const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + endpoints.downloadCalificaciones(idCurso))
+            const blob = await res.blob()
+
+            // Create a link element
+            const link = document.createElement('a');
+
+            // Create a URL for the blob
+            const url = URL.createObjectURL(blob);
+
+            // Set link properties
+            link.href = url;
+            link.download = 'calificaciones.csv';
+
+            // Append the link to the document and programmatically click it
+            document.body.appendChild(link);
+            link.click();
+
+            // Clean up: remove the link and revoke the blob URL
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+
+        } catch (e) {
+            console.error("Hubo un error al descargar las calificaciones")
+        }
+    }
 
     if (status === 'loading')
         return <Spinner color="primary" size="lg" className="justify-center items-center h-full" />
@@ -25,7 +56,20 @@ export default function Calificaciones({ params: { idCurso } }: { params: { idCu
 
     return (
         <section className="flex flex-col flex-1 p-10 gap-4 overflow-auto">
-            <PagesHeader title={isDocente ? "Calificaciones realizadas" : "Mis calificaciones"} searchable={false} />
+            <header className="flex flex-row justify-between items-center mb-4 mx-2.5">
+                <h1 className="text-2xl font-semibold self-center">{isDocente ? "Calificaciones realizadas" : "Mis calificaciones"}</h1>
+                <Tooltip
+                    showArrow={false}
+                    placement="bottom"
+                    className="max-w-[300px]"
+                    content={
+                        <p className="text-sm">Descargar todas las calificaciones</p>
+                    }>
+                    <Button isIconOnly className="bg-transparent" onClick={downloadCsv}>
+                        <DownloadIcon />
+                    </Button>
+                </Tooltip>
+            </header>
             {isDocente ?
                 <CalificacionesDocente idCurso={idCurso} idDocente={session.user.id} />
                 :
---
+++ Frontend/src/app/componentes/ui/icons/DownloadIcon.tsx
@@ -0,0 +1,3 @@
+export const DownloadIcon = () => (
+    <svg viewBox="0 0 24 24" height="25" width="25" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 3V16M12 16L16 11.625M12 16L8 11.625" stroke="#3a4e88" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M15 21H9C6.17157 21 4.75736 21 3.87868 20.1213C3 19.2426 3 17.8284 3 15M21 15C21 17.8284 21 19.2426 20.1213 20.1213C19.8215 20.4211 19.4594 20.6186 19 20.7487" stroke="#3a4e88" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+);
