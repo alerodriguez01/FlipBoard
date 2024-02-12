3b13238974bc2b3410e2de74e675610013a5c7db
elPeiretti <tomaspeiretti@gmail.com>
2024-02-12 16:20:49 -0300

    change IMG to use path instead of comp

--- Frontend/src/app/(sameLayout)/ayuda/page.tsx
+++ Frontend/src/app/(sameLayout)/ayuda/page.tsx
@@ -1,6 +1,4 @@
 import Image, { StaticImageData } from "next/image";
-import curso from "../../../../public/ayuda/cursos1.png";
-import rubrica from "../../../../public/ayuda/rubricas2.png";
 import { ReactNode } from "react";
 import { Divider } from "@nextui-org/divider";
 
@@ -18,8 +16,8 @@ const Article = ({children, id, title}: {children?: ReactNode, id: string, title
     )
 }
 
-const IMG = ({src, alt}: {src: StaticImageData, alt: string}) => {
-    return <Image src={src} alt={alt} className="px-6 py-2"/>
+const IMG = ({src, alt}: {src: string, alt: string}) => {
+    return <Image src={src} alt={alt} width={0} height={0} sizes="100vw" className="px-6 py-2" style={{width: '100%', height: 'auto'}}/>
 }
 
 const H2 = ({children, id}: {children: ReactNode, id: string}) => {
@@ -38,7 +36,7 @@ export default function Ayuda() {
                 <Article id="cursos" title="Información sobre cursos">
                     <H2 id="crear-curso">Ver y crear cursos</H2>
                     <p>En la sección Mis cursos puede ver todos los cursos de los que forma parte, ya sea como alumno o como docente. Si desea crear un curso, puede realizarlo presionando el botón “Crear nuevo curso” (1)</p>
-                    <IMG src={curso} alt="Seccion Mis cursos"/>
+                    <IMG src={"/ayuda/cursos1.png"} alt="Seccion Mis cursos"/>
                     <p>Al presionar “Crear un nuevo curso”, le aparecerá un cuadro de diálogo con la información del curso a completar.</p>
                 </Article>
                 <Article id="rubricas" title="Información sobre rúbricas">
@@ -49,7 +47,7 @@ export default function Ayuda() {
                         <li>Presionando el botón (2) podrá agregar más criterios de evaluación.</li>
                         <li>Para crear finalmente la rúbrica, puede presionar el botón “Crear rúbrica” (3).</li>
                     </UL>
-                    <IMG src={rubrica} alt="Creacion de una rubrica"/>
+                    <IMG src={"/ayuda/rubricas2.png"} alt="Creacion de una rubrica"/>
                 </Article>
             </div>
             <aside className="w-[20%] overflow-y-auto">
