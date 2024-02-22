7f7fda5a46bc176192583de2fb5f9fad21ec0cfd
elPeiretti <tomaspeiretti@gmail.com>
2024-02-22 10:38:24 -0300

    add superuser condition to Excalidraw components

--- Mural/Excalidraw/excalidraw-app/flipboard/lib/types.ts
+++ Mural/Excalidraw/excalidraw-app/flipboard/lib/types.ts
@@ -21,6 +21,7 @@ export type Usuario = {
     cursosDocente: string[];
     grupos: string[];
     token: string; // cuando hace el login, retorna el token en el body tmb
+    superUser?: boolean,
 } & Error;
 export type Session = { 
     loggedIn: boolean,
